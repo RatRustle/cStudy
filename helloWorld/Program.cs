cd7c5cf6bd36db652b65922656afed16d7a0526b
rebecca <rebacca@qq.com>
2023-03-21 11:33:28 +0800

    Initial commit

---
+++ helloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace helloWorld
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("测试成功");
+        }
+    }
+}
